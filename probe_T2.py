95463437183e08ba8ce64f09cb99e6ab362b9945
Bernardo Haab <bernardohaab@gmail.com>
2024-11-03 15:23:29 -0300

    feat: add history

--- probe_T2.py
+++ probe_T2.py
@@ -13,8 +13,10 @@ from scapy.all import sniff
 # Configurações globais e estatísticas
 statistics = {}
 history = {}
+interval_stats = {}
 stats_lock = Lock()
 history_lock = Lock()
+interval_lock = Lock()
 sampling_interval = 10  # Intervalo de amostragem para o grupo history (em segundos)
 
 oidToName = {
@@ -28,19 +30,27 @@ def create_stats_entry(interface):
     if interface not in statistics:
         statistics[interface] = {'etherStatsPkts': 0, 'etherStatsOctets': 0, 'etherStatsOversizePkts': 0, 'etherStatsBroadcastPkts': 0}
 
+def create_interval_entry(interface):
+        interval_stats[interface] = {'etherStatsPkts': 0, 'etherStatsOctets': 0, 'etherStatsOversizePkts': 0, 'etherStatsBroadcastPkts': 0}
+
 def is_broadcast(packet):
     return packet.dst == 'ff:ff:ff:ff:ff:ff'
 
 # Função para capturar pacotes em uma interface e atualizar estatísticas
 def capture_packets(interface):
     def process_packet(packet):
-        with stats_lock:
+        with stats_lock, interval_lock:
             create_stats_entry(interface)
+            create_interval_entry(interface)
+            interval_stats[interface]['etherStatsPkts'] += 1
             statistics[interface]['etherStatsPkts'] += 1
+            interval_stats[interface]['etherStatsOctets'] += len(packet)
             statistics[interface]['etherStatsOctets'] += len(packet)
             if len(packet) > 1518:
+                interval_stats[interface]['etherStatsOversizePkts'] += 1
                 statistics[interface]['etherStatsOversizePkts'] += 1
             if is_broadcast(packet):
+                interval_stats[interface]['etherStatsBroadcastPkts'] += 1
                 statistics[interface]['etherStatsBroadcastPkts'] += 1
 
     sniff(iface=interface, prn=process_packet)
@@ -69,6 +79,18 @@ def get_value(oid):
             return stats[name]
     return None, None
 
+# Função para capturar dados históricos em intervalos de tempo
+def capture_history():
+    while True:
+        time.sleep(sampling_interval)
+        timestamp = time.time()
+
+        with stats_lock, history_lock:
+            history[timestamp] = interval_stats.copy()
+            for interface in interval_stats:
+                create_interval_entry(interface)
+            print(f"History: {history}")
+
 # Função principal
 if __name__ == "__main__":
     # Recebe as interfaces da linha de comando
@@ -78,20 +100,24 @@ if __name__ == "__main__":
     print(interfaces)
     start_monitoring(interfaces)
 
-    print("OID:")
-    oid = sys.stdin.readline()
-
-    value = get_value(oid)
-    print(value)
-
 
     # monitoring_thread = Thread(target=start_monitoring, args=(interfaces,))
     # monitoring_thread.daemon = True
     # monitoring_thread.start()
 
     # Inicia a captura de histórico em uma thread separada
-    # history_thread = Thread(target=capture_history)
-    # history_thread.start()
+    history_thread = Thread(target=capture_history)
+    history_thread.daemon = True
+    history_thread.start()
+    try:
+      while True:
+        time.sleep
+    except KeyboardInterrupt:
+      print("Monitoring stopped by user.")
+    # print("OID:")
+    # oid = sys.stdin.readline()
 
+    # value = get_value(oid)
+    # print(value)
     # Inicia o servidor SNMP
     # start_snmp_server()
